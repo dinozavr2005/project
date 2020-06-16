a2d30db2a7e900bcd49019635c97fb31881bc385
Vladimir <Vladimir@gmail.com>
2020-06-16 15:26:31 +0300

    1practica

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+
 {let second = 1;
-console.log(second);
-}
-let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
-console.log(arr[0])
-
-alert('Hellow World')
-
-
-
+    console.log(second);
+    }
+    let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
+    console.log(arr[0])
+    
+    alert('Hellow World')
+    
+    
+    
 
 
